7322f61f519c21d25887f83bd661d27f5ff3872f
XGZzs0ocFyf1 <none>
2020-12-11 21:56:16 +1000

    change Datasystem::sendInvoce: remove add method to queue because it will not wait changes and throws exception if size is more than predefined. Use put instead.
    DataSystem::receive:
    remove poll method from it, and add take() instead;

--- src/main/java/myhomework/DataSystem.java
+++ src/main/java/myhomework/DataSystem.java
@@ -8,31 +8,23 @@ public class DataSystem {
     private final BlockingQueue<Request> REQUESTS = new ArrayBlockingQueue(2);
 
     public synchronized void sendInvoice(Request request){
-
-        while (REQUESTS.size() == 2){
-
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+        try {
+            REQUESTS.put(request);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-        if (REQUESTS.size() < 2){
-            REQUESTS.add(request);
-        }
+
     }
 
 
     public  Request receive(){
-        while (REQUESTS.size() == 0){
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+        Request output = null;
+        try {
+            output = REQUESTS.take();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-
-        return  REQUESTS.poll();
+        return  output;
     }
 
 
